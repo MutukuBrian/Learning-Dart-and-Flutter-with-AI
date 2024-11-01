aaec0443e1ab290c7d4d46cd445dbd7019c1caa8
BrianMutuku <andersonssecure@gmail.com>
2024-11-01 13:21:18 +0300

    New mini project

--- Dart & Flutter Mini Projects/ninja_id/lib/main.dart
+++ Dart & Flutter Mini Projects/ninja_id/lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    
+
     home:NinjaCard(),
   ));
 }
@@ -11,15 +11,115 @@ class NinjaCard extends StatelessWidget{
   @override
   Widget build (BuildContext context){
     return Scaffold(
+      backgroundColor:Colors.grey[900],
       appBar: AppBar(
         title: Text("Ninja ID card"),
-        
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0.0,
+
       ),
-      
-      
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:<Widget> [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/thumb.jpg'),
+                radius: 80,
+              ),
+
+            ),
+            Divider(
+              height: 90,
+              color: Colors.grey[800],
+
+            ),
+            Text(
+              "NAME",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+
+              ),
+
+            ),
+            SizedBox(height: 10),
+
+            Text(
+              "Chung-Li",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+
+              ),
+
+            ),
+            SizedBox(height: 30),
+
+            Text(
+              "CURRENT NINJA LEVEL",
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+
+              ),
+
+            ),
+            SizedBox(height: 10),
+
+            Text(
+              "10",
+              style: TextStyle(
+                color: Colors.amberAccent[200],
+                letterSpacing: 2.0,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+
+              ),
+
+            ),
+            SizedBox(height: 30),
+
+            Row(
+
+              children:<Widget> [
+                Icon(Icons.email,
+                  color: Colors.grey[400],
+
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "chung.li@shadowcompany.com",
+                  style: TextStyle(
+                    color: Colors.grey[400],
+                    fontSize: 18,
+                    letterSpacing: 1,
+                  )
+                ),
+
+
+              ],
+            )
+
+
+
+          ],
+
+
+        ),
+
+
+
+
+      ),
+
     );
-    
-    
+
+
   }
-  
+
 }
