d05648997fff7db9f0970cbfba86d4f1b8924f3d
BrianMutuku <andersonssecure@gmail.com>
2024-11-01 11:17:18 +0300

    New mini project

---
+++ Dart & Flutter Mini Projects/ninja_id/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    
+    home:NinjaCard(),
+  ));
+}
+
+class NinjaCard extends StatelessWidget{
+  @override
+  Widget build (BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ninja ID card"),
+        
+      ),
+      
+      
+    );
+    
+    
+  }
+  
+}
