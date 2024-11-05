abb41b956c50ae7258c49decb97aeca38646aa0c
BrianMutuku <andersonssecure@gmail.com>
2024-11-05 18:55:37 +0300

    Finished flutter tutorial

--- DartFlutterMiniProjects/world_time/lib/pages/home.dart
+++ DartFlutterMiniProjects/world_time/lib/pages/home.dart
@@ -6,24 +6,90 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  Map<String, dynamic> data = {};
 
   @override
   Widget build(BuildContext context) {
+    // Using the safe navigation operator to handle potential null values
+    final routeData = ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+
+    // Set data to the passed arguments or keep it unchanged if not available
+    data = routeData ?? data;
+
+    // Provide default values if the data map doesn't contain expected keys
+    String location = data['location'] ?? 'Unknown location';
+    String time = data['time'] ?? 'Unknown time';
+    bool isDaytime = data['isDaytime'] ?? true; // Default to true to avoid null issues
+    String flag = data['flag'] ?? 'default_flag.png';
+
+    // set background image
+    String bgImage = isDaytime ? 'day.png' : 'night.png';
+    Color bgColor = isDaytime ? Colors.blue : Colors.indigo[700]!;
+
     return Scaffold(
+      backgroundColor: bgColor,
       body: SafeArea(
-        child: Column(
-          children: <Widget>[
-
-        TextButton.icon(
-         onPressed: () {
-          Navigator.pushNamed(context, '/location');
-          },
-        icon: Icon(Icons.edit_location),
-        label: Text('Edit Location'),
-        )
-
-
-      ],
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/$bgImage'),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(0, 120.0, 0, 0),
+            child: Column(
+              children: <Widget>[
+                TextButton.icon(
+                  onPressed: () async {
+                    dynamic result = await Navigator.pushNamed(context, '/location');
+                    if (result != null) {
+                      setState(() {
+                        data = {
+                          'time': result['time'],
+                          'location': result['location'],
+                          'isDaytime': result['isDaytime'],
+                          'flag': result['flag']
+                        };
+                      });
+                    }
+                  },
+                  icon: Icon(
+                    Icons.edit_location,
+                    color: Colors.grey[300],
+                  ),
+                  label: Text(
+                    'Edit Location',
+                    style: TextStyle(
+                      color: Colors.grey[300],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      location,
+                      style: TextStyle(
+                        fontSize: 28.0,
+                        letterSpacing: 2.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20.0),
+                Text(
+                  time,
+                  style: TextStyle(
+                    fontSize: 66.0,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- DartFlutterMiniProjects/world_time/lib/pages/loading.dart
+++ DartFlutterMiniProjects/world_time/lib/pages/loading.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'dart:convert';
+import 'package:world_time/services/world_time.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -8,28 +7,29 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-  void getData() async {
-    Response response = await get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
-    print(response.body);
-
-    Map<String, dynamic> data = jsonDecode(response.body);
-    print(data);
-
-    print(data['title']);
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+    });
   }
-
 
   @override
   void initState() {
     super.initState();
-    getData();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('loading screen'),
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Center(child: Text('Loading...')),
+      ),
     );
   }
 }
